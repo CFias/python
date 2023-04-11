c69ef869fba50b96b0b239dd0a367cffe8e7a5a1
Cleidson Fias <cleidson.fias@hotmail.com>
2023-04-10 23:58:09 -0300

    Create variaveis.py
    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+idade = 25
+nome = 'Cleidson Fias'
+altura = 1.80
+estudando = True
+
+print( type(idade))
+print( type(nome))
+print( type(altura))
+print( type(estudando))
+
+linguagem = input('Qual a linguagem de programação que você está estudando ?')
+
+print ('A linguagem de programação que você está estudando é', linguagem) 
